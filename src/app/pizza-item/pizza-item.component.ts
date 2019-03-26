c892488165ad4f5c3b2e1bfb70d56cc41e596b73
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2019-03-26 17:54:40 +0300

    Fixed price calculate

--- src/app/pizza-item/pizza-item.component.ts
+++ src/app/pizza-item/pizza-item.component.ts
@@ -1,59 +1,59 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { Pizza } from "../pizzas";
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Pizza } from '../pizzas';
 
 @Component({
-  selector: "app-pizza-item",
-  templateUrl: "./pizza-item.component.html",
-  styleUrls: ["./pizza-item.component.scss"]
+  selector: 'app-pizza-item',
+  templateUrl: './pizza-item.component.html',
+  styleUrls: ['./pizza-item.component.scss']
 })
 export class PizzaItemComponent implements OnInit {
   @Input() pizza: any;
 
   selectedDough: string;
-  selectedSize: number = 0;
-  price: number = 0;
-  isAddCheese: boolean = false;
+  selectedSize = 0;
+  price = 0;
+  isAddCheese = false;
 
   sizes: Array<number> = [];
   ingredients: Array<string> = [];
 
-  constructor() {}
+  constructor() { }
 
-  handleChangeDough(evt) {
+  handleChangeDough (evt) {
     this.selectedDough = evt.value;
   }
-  handleChangeSize(size) {
+  handleChangeSize (size) {
     this.selectedSize = size;
   }
-  handleChangeCheese(isChecked) {
+  handleChangeCheese (isChecked) {
     if (isChecked) {
       this.isAddCheese = true;
       return;
     }
     this.isAddCheese = false;
   }
-  getTotalPrice() {
+  getTotalPrice () {
     this.pizza.info.map((item, index) => {
       if (this.selectedSize === item.size) {
         this.price = item.price;
       }
-      if (this.selectedSize === item.size && this.selectedDough === "thin") {
+      if (this.selectedSize === item.size && this.selectedDough === 'thin') {
         this.price = item.price;
       }
       if (
         this.selectedSize === item.size &&
-        this.selectedDough === "traditional"
+        this.selectedDough === 'traditional'
       ) {
         this.price = item.price + 2;
       }
-      if (this.selectedSize === item.size && this.isAddCheese) {
+      if (this.selectedSize !== 23 && this.selectedSize === item.size && this.isAddCheese) {
         this.price = +(item.price * 0.15 + this.price).toFixed(2);
       }
     });
 
     return this.price;
   }
-  ngOnInit() {
+  ngOnInit () {
     this.pizza.info.map((item, index) => {
       this.sizes.push(item.size);
     });
