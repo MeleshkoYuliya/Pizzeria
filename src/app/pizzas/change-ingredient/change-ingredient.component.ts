339556101c4daffd31d03680bca661be5cc8f60b
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2019-04-11 15:27:36 +0300

    Added new modal window with directive modal, change-ingredient component

---
+++ src/app/pizzas/change-ingredient/change-ingredient.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-change-ingredient',
+  templateUrl: './change-ingredient.component.html',
+  styleUrls: ['./change-ingredient.component.scss']
+})
+export class ChangeIngredientComponent implements OnInit {
+  @Input() ingredients: Array<any>;
+  constructor() { }
+  ngOnInit () {
+  }
+
+}
--- src/app/pizzas/modal/modal-container.ts
+++
@@ -1,6 +0,0 @@
-import { ModalContext } from './modal-context';
-import { ViewContainerRef } from '@angular/core';
-export interface ModalContainer {
-  context: ModalContext<any>;
-  container: ViewContainerRef;
-}
