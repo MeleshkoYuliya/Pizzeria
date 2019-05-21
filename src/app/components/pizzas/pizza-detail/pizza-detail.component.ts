1242cad12c1095fa22827bfa13f52201972ebee2
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2019-05-21 12:25:50 +0300

    Fixed code

--- src/app/components/pizzas/pizza-detail/pizza-detail.component.ts
+++ src/app/components/pizzas/pizza-detail/pizza-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Store } from '@ngxs/store';
+import { Select } from '@ngxs/store';
+import { PizzasState } from '../../../store/state/pizzas.state';
 
 import { Pizza } from '../../../models/pizza.model';
 
@@ -16,7 +18,7 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PizzaDetailComponent implements OnInit {
-  priceClass = 'price-card';
+  @Select(PizzasState.getPizzas) pizzas$: Observable<Pizza[]>;
   pizza: Observable<Pizza>;
 
   constructor(
@@ -33,7 +35,7 @@ export class PizzaDetailComponent implements OnInit {
     }
 
     this.store.dispatch(new GetPizzas());
-    this.pizza = this.store.select(state => state.pizzas.pizzas).pipe(
+    this.pizza = this.pizzas$.pipe(
       map((pizzas: Pizza[]) => pizzas.find(pizza => pizza.id === +id))
     );
   }
