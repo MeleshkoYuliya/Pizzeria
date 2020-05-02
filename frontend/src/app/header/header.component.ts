f4104bc6bae00d6dcb8376bea886a4aa0e385372
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2020-05-02 15:50:55 +0300

    Fixed header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["../app.component.scss"]
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+constructor(private auth: AuthService){}
+isAuthenticated: boolean = this.auth.isAuthenticated();
+
+ngOnInit(){
+  console.log(this.isAuthenticated);
+  
+  // this.isAuthenticated = this.auth.isAuthenticated()
+}
+}
