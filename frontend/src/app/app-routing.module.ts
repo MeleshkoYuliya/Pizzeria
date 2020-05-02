7b35c46cde4b6557fda224031616e8f4cda4a4ba
Yuliya Meleshko <yuliya.meleshko@itechart-group.com>
2020-05-02 15:50:36 +0300

    Added imports and routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { PizzasComponent } from './pizzas/pizzas.component';
 import { CheckoutComponent } from './order/checkout/checkout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  { path: '', redirectTo: '/pizzas', pathMatch: 'full' },
   { path: 'pizzas', component: PizzasComponent },
   { path: 'checkout', component: CheckoutComponent },
-  { path: '', redirectTo: '/pizzas', pathMatch: 'full' },
   { path: 'not-found', component: PageNotFoundComponent },
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,9 +16,19 @@ import { CheckoutComponent } from './order/checkout/checkout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { PizzasState } from './pizzas/pizzas.state';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { TokenInterceptor } from './shared/classes/token.interceptor';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CheckoutComponent, PageNotFoundComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CheckoutComponent,
+    PageNotFoundComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -30,7 +40,13 @@ import { PizzasState } from './pizzas/pizzas.state';
     NgxsReduxDevtoolsPluginModule.forRoot(),
     NgxsLoggerPluginModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
