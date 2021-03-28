5a8cf1739bd8b849caeed82585408b713b1502ac
Julia <julia.m@5ly.co>
2021-03-28 17:55:26 +0300

    Added cart badge

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy  } from "@angular/core";
 import {Router} from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["../app.component.scss"]
 })
-export class HeaderComponent {
-constructor(private auth: AuthService,  private router: Router){}
+export class HeaderComponent implements OnInit{
+  pizzasAmount: number = 0;
+constructor(private auth: AuthService,  private router: Router, private store: Store){}
 
+ngOnInit() {
+  this.store.select(state => state.pizzas.orderedPizzas).subscribe(pizzas => {this.pizzasAmount = pizzas.length })
+}
+    
 checkAuth (){
   return this.auth.isAuthenticated();
 }
